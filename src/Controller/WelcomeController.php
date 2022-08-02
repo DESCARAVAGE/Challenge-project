cca8f9ec1242802076c097f5947f59a040b63db9
daniel <daniel@daniel-Inspiron-7786>
2022-08-02 15:31:06 +0200

    début du style

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class WelcomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'welcome')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('welcome/index.html.twig');
     }
 }
