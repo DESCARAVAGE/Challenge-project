acdf11132ec99112c6b24e5da597acd62861af9b
daniel <daniel@daniel-Inspiron-7786>
2022-08-10 08:08:48 +0200

    fixtures et view

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\LanguageRepository;
+use App\Repository\LevelRepository;
 use App\Repository\TypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class WelcomeController extends AbstractController
 {
     #[Route('/', name: 'welcome')]
-    public function index(TypeRepository $typeRepository): Response
-    {
+    public function index(
+        TypeRepository $typeRepository,
+        LanguageRepository $languageRepository,
+        LevelRepository $levelRepository
+    ): Response {
         $types = $typeRepository->findAll();
+        $languages = $languageRepository->findAll();
+        $levels = $levelRepository->findAll();
         return $this->render('welcome/index.html.twig', [
             'types' => $types,
+            'languages' => $languages,
+            'levels' => $levels,
         ]);
     }
 }
